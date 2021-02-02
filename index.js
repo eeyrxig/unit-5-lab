cd2392fe3c4f723716dc995bb09b282d4550adfd
eeyrxig <hernandezcat7@gmail.com>
2021-02-02 13:02:33 -0500

    added player

--- index.js
+++ index.js
@@ -1,19 +1,70 @@
 document.body.style.backgroundColor = "#DEAAFF"                       
-// let row1A = document.querySelector("#sec1")
-// let row1B= document.querySelector("#sec2")
-// let row1C = document.querySelector("#sec3")
-// let row2A = document.querySelector("#sec4")
-// let row2B = document.querySelector("#sec5")
-// let row2C = document.querySelector("#sec6")
-// let row3A = document.querySelector("#sec7")
-// let row3B = document.querySelector("#sec8")
-// let row3C = document.querySelector("#sec9")
 let allBoxes = document.getElementsByClassName("box")
+
+let currentPlayer = "X";
+let gameStatus = "Game On";
 
 for (let i = 0; i < allBoxes.length; i++) {
     allBoxes[i].addEventListener("click", () => {
-        allBoxes[i].innerText = "x"
-        
+        //allBoxes[i].innerText = "x"
+        if (allBoxes[i].innerHTML.trim() === "" && gameStatus === "Game On") {
+            allBoxes[i].innerHTML =currentPlayer;
+
+            currentPlayer = currentPlayer === "x" ? "o" : "x";
+
+            document.getElementById("player").innerHTML = currentPlayer.toUpperCase();
+
+            if (
+              allBoxes[0].innerHTML == allBoxes[1].innerHTML &&
+              allBoxes[1].innerHTML == allBoxes[2].innerHTML &&
+              allBoxes[0].innerHTML.trim() != ""
+            ) {
+              showWinner(0, 1, 2);
+            } else if (
+              allBoxes[3].innerHTML == allBoxes[4].innerHTML &&
+              allBoxes[4].innerHTML == allBoxes[5].innerHTML &&
+              allBoxes[3].innerHTML.trim() != ""
+            ) {
+              showWinner(3, 4, 5);
+            } else if (
+              allBoxes[6].innerHTML == allBoxes[7].innerHTML &&
+              allBoxes[7].innerHTML == allBoxes[8].innerHTML &&
+              allBoxes[6].innerHTML.trim() != ""
+            ) {
+              showWinner(6, 7, 8);
+            } else if (
+              allBoxes[0].innerHTML == allBoxes[3].innerHTML &&
+              allBoxes[3].innerHTML == allBoxes[6].innerHTML &&
+              allBoxes[0].innerHTML.trim() != ""
+            ) {
+              showWinner(0, 3, 6);
+            } else if (
+              allBoxes[1].innerHTML == allBoxes[4].innerHTML &&
+              allBoxes[4].innerHTML == allBoxes[7].innerHTML &&
+              allBoxes[1].innerHTML.trim() != ""
+            ) {
+              showWinner(1, 4, 7);
+            } else if (
+              allBoxes[2].innerHTML == allBoxes[5].innerHTML &&
+              allBoxes[5].innerHTML == allBoxes[8].innerHTML &&
+              allBoxes[2].innerHTML.trim() != ""
+            ) {
+              showWinner(2, 5, 8);
+            } else if (
+              allBoxes[0].innerHTML == allBoxes[4].innerHTML &&
+              allBoxes[4].innerHTML == allBoxes[8].innerHTML &&
+              allBoxes[0].innerHTML.trim() != ""
+            ) {
+              showWinner(0, 4, 8);
+            } else if (
+              allBoxes[2].innerHTML == allBoxes[4].innerHTML &&
+              allBoxes[4].innerHTML == allBoxes[6].innerHTML &&
+              allBoxes[2].innerHTML.trim() != ""
+            ) {
+              showWinner(2, 4, 6);
+            }
+            
+        }
     })
 }
 
